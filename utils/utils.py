c0d1aad97e21dacf879514823d22ffc73e0530ed
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-08-26 19:38:14 +0200

    updates

--- utils/utils.py
+++ utils/utils.py
@@ -361,10 +361,11 @@ def plotResults():
     import matplotlib.pyplot as plt
     plt.figure(figsize=(18, 9))
     s = ['x', 'y', 'w', 'h', 'conf', 'cls', 'loss', 'prec', 'recall']
-    for f in ('results.txt',):
+    for f in ('/Users/glennjocher/Downloads/results.txt',
+              'results.txt'):
         results = np.loadtxt(f, usecols=[2, 3, 4, 5, 6, 7, 8, 9, 10]).T
         for i in range(9):
             plt.subplot(2, 5, i + 1)
-            plt.plot(results[i, :3000], marker='.', label=f)
+            plt.plot(results[i, :], marker='.', label=f)
             plt.title(s[i])
         plt.legend()
