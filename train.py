e7cd5d01c409132d0e5ae1393ff1eca68ff244c9
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-10-09 19:28:27 +0200

    cleanup train.py

--- train.py
+++ train.py
@@ -156,8 +156,8 @@ def main(opt):
             s = ('%10s%10s' + '%10.3g' * 14) % (
                 '%g/%g' % (epoch, opt.epochs - 1), '%g/%g' % (i, len(dataloader) - 1), rloss['x'],
                 rloss['y'], rloss['w'], rloss['h'], rloss['conf'], rloss['cls'],
-                rloss['loss'], mean_precision, mean_recall, model.losses['nT'], TP.sum(),
-                FP.sum(), FN.sum(), time.time() - t1)
+                rloss['loss'], mean_precision, mean_recall, model.losses['nT'], rloss['TP'],
+                rloss['FP'], rloss['FN'], time.time() - t1)
             t1 = time.time()
             print(s)
 
