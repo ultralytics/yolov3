8d7660b43802b0d1097e3bd01546d2531935f497
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-11-07 15:17:00 +0100

    updates

--- utils/gcp.sh
+++ utils/gcp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Start
-sudo rm -rf yolov3 && git clone https://github.com/ultralytics/yolov3 && cd yolov3 && python3 train.py -batch_size 8
+sudo rm -rf yolov3 && git clone https://github.com/ultralytics/yolov3 && cd yolov3 && python3 train.py
 
 # Resume
 python3 train.py -resume 1
@@ -14,18 +14,15 @@ python3 detect.py
 python3 test.py -img_size 416 -weights_path weights/latest.pt -conf_thres 0.5
 
 # Download and Test
-sudo rm -rf yolov3 && git clone https://github.com/ultralytics/yolov3
-cd yolov3/weights
-wget https://pjreddie.com/media/files/yolov3.weights
-cd ..
+sudo rm -rf yolov3 && git clone https://github.com/ultralytics/yolov3 && cd yolov3
+wget https://pjreddie.com/media/files/yolov3.weights -P weights
 python3 test.py -img_size 416 -weights_path weights/backup5.pt -nms_thres 0.45
 
 # Download and Resume
-sudo rm -rf yolov3 && git clone https://github.com/ultralytics/yolov3
-cd yolov3/weights
-wget https://storage.googleapis.com/ultralytics/yolov3.pt
-cp yolov3.pt latest.pt
-cd ..
+sudo rm -rf yolov3 && git clone https://github.com/ultralytics/yolov3 && cd yolov3
+wget https://storage.googleapis.com/ultralytics/yolov3.pt -O weights/latest.pt
 python3 train.py -img_size 416 -batch_size 16 -epochs 1 -resume 1
 python3 test.py -img_size 416 -weights_path weights/latest.pt -conf_thres 0.5
 
+# Copy latest.pt to bucket
+gsutil cp yolov3/weights/latest.pt gs://ultralytics
