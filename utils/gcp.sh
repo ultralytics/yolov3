8ac8d0a38283a55f1ee2fade62394bba4c52845e
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-11-21 19:22:35 +0100

    updates

--- utils/gcp.sh
+++ utils/gcp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Start
-sudo rm -rf yolov3 && git clone https://github.com/ultralytics/yolov3 && cd yolov3 && python3 train.py -epochs 2
+sudo rm -rf yolov3 && git clone https://github.com/ultralytics/yolov3 && cd yolov3 && python3 train.py
 
 # Resume
 python3 train.py -resume 1
