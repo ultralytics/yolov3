682fd61385dcb8f51a65153e1a7254f80514aeff
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-12-17 22:43:30 +0100

    updates

--- weights/download_yolov3_weights.sh
+++ weights/download_yolov3_weights.sh
@@ -2,4 +2,5 @@
 
 wget https://pjreddie.com/media/files/darknet53.conv.74
 wget https://pjreddie.com/media/files/yolov3.weights
+wget https://pjreddie.com/media/files/yolov3-tiny.weights
 wget https://storage.googleapis.com/ultralytics/yolov3.pt
