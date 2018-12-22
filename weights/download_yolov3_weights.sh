465f8476609df387fa10756cec923b2cc1829036
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-12-22 13:54:02 +0100

    updates

--- weights/download_yolov3_weights.sh
+++ weights/download_yolov3_weights.sh
@@ -9,8 +9,8 @@ wget -c https://pjreddie.com/media/files/yolov3.weights
 wget -c https://pjreddie.com/media/files/yolov3-tiny.weights
 
 # yolov3 pytorch weights
-wget https://storage.googleapis.com/ultralytics/yolov3.pt
-wget https://storage.googleapis.com/ultralytics/yolov3-tiny.pt
+wget -c https://storage.googleapis.com/ultralytics/yolov3.pt
+wget -c https://storage.googleapis.com/ultralytics/yolov3-tiny.pt
 
 # darknet53 weights (first 75 layers only)
-wget https://pjreddie.com/media/files/darknet53.conv.74
+wget -c https://pjreddie.com/media/files/darknet53.conv.74
