16bc3b72c333e57f431f42028c0b41115cba476f
Glenn Jocher <glenn.jocher@ultralytics.com>
2018-12-28 20:11:10 +0100

    updates

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -13,6 +13,7 @@ def init_seeds(seed=0):
     if CUDA_AVAILABLE:
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+        # torch.cuda.set_device(0)  # OPTIONAL: Set your GPU if multiple available
 
 
 def select_device(force_cpu=False):
