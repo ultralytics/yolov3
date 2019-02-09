e88798aefdf0beafc48e067863cb6bc9d329c2b5
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-02-09 18:52:02 +0100

    updates

--- weights/download_yolov3_weights.sh
+++ weights/download_yolov3_weights.sh
@@ -3,14 +3,13 @@
 # make '/weights' directory if it does not exist and cd into it
 mkdir -p weights && cd weights
 
-# copy weight files, continue '-c' if partially downloaded
-# yolov3 darknet weights
+# copy darknet weight files, continue '-c' if partially downloaded
 wget -c https://pjreddie.com/media/files/yolov3.weights
 wget -c https://pjreddie.com/media/files/yolov3-tiny.weights
 wget -c https://pjreddie.com/media/files/yolov3-spp.weights
 
 # yolov3 pytorch weights
-# download from Google Drive: https://drive.google.com/drive/u/0/folders/1uxgUBemJVw9wZsdpboYbzUN4bcRhsuAI
+# download from Google Drive: https://drive.google.com/drive/folders/1uxgUBemJVw9wZsdpboYbzUN4bcRhsuAI
 
 # darknet53 weights (first 75 layers only)
 wget -c https://pjreddie.com/media/files/darknet53.conv.74
