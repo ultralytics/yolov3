6f58e1384a6f2268608af5ae569490eda16c67e4
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-02-11 13:47:58 +0100

    class labeling corrections

--- detect.py
+++ detect.py
@@ -99,8 +99,8 @@ def detect(
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='cfg/yolov3-tiny.cfg', help='cfg file path')
-    parser.add_argument('--weights', type=str, default='weights/yolov3-tiny.pt', help='path to weights file')
+    parser.add_argument('--cfg', type=str, default='cfg/yolov3.cfg', help='cfg file path')
+    parser.add_argument('--weights', type=str, default='weights/yolov3.pt', help='path to weights file')
     parser.add_argument('--images', type=str, default='data/samples', help='path to images')
     parser.add_argument('--img-size', type=int, default=32 * 13, help='size of each image dimension')
     parser.add_argument('--conf-thres', type=float, default=0.50, help='object confidence threshold')
