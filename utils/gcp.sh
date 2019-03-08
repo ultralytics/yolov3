47d0276863eba96d8289b8deec8466ced2efcdad
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-03-08 18:45:47 +0100

    updates

--- utils/gcp.sh
+++ utils/gcp.sh
@@ -17,6 +17,7 @@ python3 detect.py
 
 # Train new branch
 sudo rm -rf yolov3 && git clone -b multi_gpu --depth 1 https://github.com/ultralytics/yolov3
+sudo rm -rf yolov3 && git clone -b multigpu --depth 1 https://github.com/alexpolichroniadis/yolov3
 cd yolov3 && python3 train.py --batch-size 16
 
 # Test
