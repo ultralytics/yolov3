b7c5eb1503b26572b740658668dd520dc5e2c070
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-04-18 16:07:47 +0200

    updates

--- utils/gcp.sh
+++ utils/gcp.sh
@@ -43,6 +43,17 @@ gsutil cp gs://ultralytics/latest.pt yolov3/weights/latest.pt
 wget https://storage.googleapis.com/ultralytics/yolov3/latest_v1_0.pt -O weights/latest_v1_0.pt
 wget https://storage.googleapis.com/ultralytics/yolov3/best_v1_0.pt -O weights/best_v1_0.pt
 
+# Reproduce tutorials
+rm results*.txt  # WARNING: removes existing results
+python3 train.py --nosave --data data/coco_1img.data && mv results.txt results3_1img.txt
+python3 train.py --nosave --data data/coco_10img.data && mv results.txt results3_10img.txt
+python3 train.py --nosave --data data/coco_100img.data && mv results.txt results4_100img.txt
+python3 train.py --nosave --data data/coco_100img.data --transfer && mv results.txt results3_100imgTL.txt
+python3 -c "from utils import utils; utils.plot_results()"
+gsutil cp results*.txt gs://ultralytics
+gsutil cp results.png gs://ultralytics
+sudo shutdown
+
 # Debug/Development
 sudo rm -rf yolov3
 git clone https://github.com/ultralytics/yolov3  # master
@@ -50,24 +61,8 @@ git clone https://github.com/ultralytics/yolov3  # master
 cp -r weights yolov3
 cp -r cocoapi/PythonAPI/pycocotools yolov3
 cd yolov3
-python3 train.py --nosave --data data/coco_32img.data --var 4 && mv results.txt results_t2.txt
-python3 train.py --nosave --data data/coco_32img.data --var 5 && mv results.txt results_t3.txt
-python3 -c "from utils import utils; utils.plot_results()"
-gsutil cp results*.txt gs://ultralytics
-gsutil cp results.png gs://ultralytics
-sudo shutdown
+python3 train.py --evolve --data data/coco_100img.data --batch-size 8 --epochs 100
 
-mv ../train.py .
 
-rm results*.txt  # WARNING: removes existing results
-python3 train.py --nosave --data data/coco_1img.data && mv results.txt results3_1img.txt
-python3 train.py --nosave --data data/coco_10img.data && mv results.txt results3_10img.txt
-python3 train.py --nosave --data data/coco_100img.data && mv results.txt results4_100img.txt
-python3 train.py --nosave --data data/coco_100img.data --transfer && mv results.txt results3_100imgTL.txt
-# python3 train.py --nosave --data data/coco_1000img.data && mv results.txt results_1000img.txt
-python3 -c "from utils import utils; utils.plot_results()"
-gsutil cp results*.txt gs://ultralytics
-gsutil cp results.png gs://ultralytics
-sudo shutdown
 
 
