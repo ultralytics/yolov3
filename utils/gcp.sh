68490520db1e49673e8dfe39979821318f80e22c
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-05-19 17:28:14 +0200

    updates

--- utils/gcp.sh
+++ utils/gcp.sh
@@ -67,6 +67,8 @@ python3 train.py --data data/coco_1cls.data --epochs 5 --nosave  # train 5 epoch
 python3 train.py --data data/coco_1img.data --epochs 5 --nosave  # train 5 epochs
 
 # AlexyAB Darknet
+rm -rf darknet && git clone https://github.com/AlexeyAB/darknet
+wget -c https://pjreddie.com/media/files/darknet53.conv.74
 ./darknet detector train ../supermarket2/supermarket2.data cfg/yolov3-spp-sm2.cfg darknet53.conv.74 # train
 ./darknet detector train ../supermarket2/supermarket2.data cfg/yolov3-spp.cfg backup/yolov3-spp_last.weights  # resume
 python3 test.py --data ../supermarket2/supermarket2.data --weights ../darknet/backup yolov3-spp_3000.weights  # test
