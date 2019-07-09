7b8a134a0bde17ed3a804644f8eb64798e9212a7
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-07-09 18:16:35 +0200

    updates

--- utils/utils.py
+++ utils/utils.py
@@ -627,7 +627,7 @@ def plot_images(imgs, targets, paths=None, fname='images.jpg'):
         boxes[[0, 2]] *= w
         boxes[[1, 3]] *= h
         plt.subplot(ns, ns, i + 1).imshow(imgs[i].transpose(1, 2, 0))
-        plt.plot(boxes[[0, 2, 2, 0, 0]], boxes[[1, 1, 3, 3, 1]], 'w.-')
+        plt.plot(boxes[[0, 2, 2, 0, 0]], boxes[[1, 1, 3, 3, 1]], '.-')
         plt.axis('off')
         if paths is not None:
             s = Path(paths[i]).name
