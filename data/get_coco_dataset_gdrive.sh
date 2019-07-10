f02ac891227b6d5c9d41bd8096e9091d7434103d
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-07-10 12:00:06 +0200

    updates

--- data/get_coco_dataset_gdrive.sh
+++ data/get_coco_dataset_gdrive.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/48133080/how-to-download-a-google-drive-url-via-curl-or-wget/48133859
 
-# Download COCO dataset
-fileid="1HaXkef9z6y5l4vUnCYgdmEAj61c6bfWO"
-filename="coco_gdrive.zip"
+# Set fileid and filename
+filename="coco.zip"
+fileid="1HaXkef9z6y5l4vUnCYgdmEAj61c6bfWO"  # coco2014.zip
+# filename="coco.tar.gz"
+# fileid="1HaXkef9z6y5l4vUnCYgdmEAj61c6bfWO"  # coco.tar.gz
+
+# Download from Google Drive, accepting presented query
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+rm ./cookie
 
 # Unzip
-unzip -q coco_gdrive.zip
+unzip -q ${filename}
+# tar -xzf ${filename}
