dbb2cbe0d595593d47cc286d75090e6583cb417e
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-08-16 01:31:59 +0200

    updates

--- utils/gcp.sh
+++ utils/gcp.sh
@@ -5,7 +5,8 @@ rm -rf sample_data yolov3 darknet apex coco cocoapi knife knifec
 git clone https://github.com/ultralytics/yolov3
 git clone https://github.com/AlexeyAB/darknet && cd darknet && make GPU=1 CUDNN=1 CUDNN_HALF=1 OPENCV=1 && wget -c https://pjreddie.com/media/files/darknet53.conv.74 && cd ..
 git clone https://github.com/NVIDIA/apex && cd apex && pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" . --user && cd .. && rm -rf apex
-git clone https://github.com/cocodataset/cocoapi && cd cocoapi/PythonAPI && make && cd ../.. && cp -r cocoapi/PythonAPI/pycocotools yolov3
+#git clone https://github.com/cocodataset/cocoapi && cd cocoapi/PythonAPI && make && cd ../.. && cp -r cocoapi/PythonAPI/pycocotools yolov3
+sudo conda install -y -c conda-forge scikit-image tensorboard pycocotools
 python3 -c "
 from yolov3.utils.google_utils import gdrive_download
 gdrive_download('1HaXkef9z6y5l4vUnCYgdmEAj61c6bfWO','coco.zip')
@@ -17,7 +18,7 @@ sudo shutdown
 rm -rf yolov3  # Warning: remove existing
 git clone https://github.com/ultralytics/yolov3  # master
 # git clone -b test --depth 1 https://github.com/ultralytics/yolov3 test  # branch
-cp -r cocoapi/PythonAPI/pycocotools yolov3
+# cp -r cocoapi/PythonAPI/pycocotools yolov3
 cp -r weights yolov3 && cd yolov3
 
 # Train
