ee156903d7f83806bf2326206a18be3948b134b9
yonghye <robotmanyh@gmail.com>
2019-08-16 19:38:20 +0900

    cleanup

--- test.py
+++ test.py
@@ -59,91 +59,93 @@ def test(cfg,
     p, r, f1, mp, mr, map, mf1 = 0., 0., 0., 0., 0., 0., 0.
     loss = torch.zeros(3)
     jdict, stats, ap, ap_class = [], [], [], []
-    for batch_i, (imgs, targets, paths, shapes) in enumerate(tqdm(dataloader, desc=s)):
-        targets = targets.to(device)
-        imgs = imgs.to(device)
-        _, _, height, width = imgs.shape  # batch size, channels, height, width
-
-        # Plot images with bounding boxes
-        if batch_i == 0 and not os.path.exists('test_batch0.jpg'):
-            plot_images(imgs=imgs, targets=targets, paths=paths, fname='test_batch0.jpg')
-
-        # Run model
-        inf_out, train_out = model(imgs)  # inference and training outputs
-
-        # Compute loss
-        if hasattr(model, 'hyp'):  # if model has loss hyperparameters
-            loss += compute_loss(train_out, targets, model)[1][[0, 2, 3]].cpu()  # GIoU, obj, cls
-
-        # Run NMS
-        output = non_max_suppression(inf_out, conf_thres=conf_thres, nms_thres=nms_thres)
-
-        # Statistics per image
-        for si, pred in enumerate(output):
-            labels = targets[targets[:, 0] == si, 1:]
-            nl = len(labels)
-            tcls = labels[:, 0].tolist() if nl else []  # target class
-            seen += 1
-
-            if pred is None:
+    
+    with torch.no_grad():
+        for batch_i, (imgs, targets, paths, shapes) in enumerate(tqdm(dataloader, desc=s)):
+            targets = targets.to(device)
+            imgs = imgs.to(device)
+            _, _, height, width = imgs.shape  # batch size, channels, height, width
+    
+            # Plot images with bounding boxes
+            if batch_i == 0 and not os.path.exists('test_batch0.jpg'):
+                plot_images(imgs=imgs, targets=targets, paths=paths, fname='test_batch0.jpg')
+    
+            # Run model
+            inf_out, train_out = model(imgs)  # inference and training outputs
+    
+            # Compute loss
+            if hasattr(model, 'hyp'):  # if model has loss hyperparameters
+                loss += compute_loss(train_out, targets, model)[1][[0, 2, 3]].cpu()  # GIoU, obj, cls
+    
+            # Run NMS
+            output = non_max_suppression(inf_out, conf_thres=conf_thres, nms_thres=nms_thres)
+    
+            # Statistics per image
+            for si, pred in enumerate(output):
+                labels = targets[targets[:, 0] == si, 1:]
+                nl = len(labels)
+                tcls = labels[:, 0].tolist() if nl else []  # target class
+                seen += 1
+    
+                if pred is None:
+                    if nl:
+                        stats.append(([], torch.Tensor(), torch.Tensor(), tcls))
+                    continue
+    
+                # Append to text file
+                # with open('test.txt', 'a') as file:
+                #    [file.write('%11.5g' * 7 % tuple(x) + '\n') for x in pred]
+    
+                # Append to pycocotools JSON dictionary
+                if save_json:
+                    # [{"image_id": 42, "category_id": 18, "bbox": [258.15, 41.29, 348.26, 243.78], "score": 0.236}, ...
+                    image_id = int(Path(paths[si]).stem.split('_')[-1])
+                    box = pred[:, :4].clone()  # xyxy
+                    scale_coords(imgs[si].shape[1:], box, shapes[si])  # to original shape
+                    box = xyxy2xywh(box)  # xywh
+                    box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
+                    for di, d in enumerate(pred):
+                        jdict.append({'image_id': image_id,
+                                      'category_id': coco91class[int(d[6])],
+                                      'bbox': [floatn(x, 3) for x in box[di]],
+                                      'score': floatn(d[4], 5)})
+    
+                # Clip boxes to image bounds
+                clip_coords(pred, (height, width))
+    
+                # Assign all predictions as incorrect
+                correct = [0] * len(pred)
                 if nl:
-                    stats.append(([], torch.Tensor(), torch.Tensor(), tcls))
-                continue
-
-            # Append to text file
-            # with open('test.txt', 'a') as file:
-            #    [file.write('%11.5g' * 7 % tuple(x) + '\n') for x in pred]
-
-            # Append to pycocotools JSON dictionary
-            if save_json:
-                # [{"image_id": 42, "category_id": 18, "bbox": [258.15, 41.29, 348.26, 243.78], "score": 0.236}, ...
-                image_id = int(Path(paths[si]).stem.split('_')[-1])
-                box = pred[:, :4].clone()  # xyxy
-                scale_coords(imgs[si].shape[1:], box, shapes[si])  # to original shape
-                box = xyxy2xywh(box)  # xywh
-                box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
-                for di, d in enumerate(pred):
-                    jdict.append({'image_id': image_id,
-                                  'category_id': coco91class[int(d[6])],
-                                  'bbox': [floatn(x, 3) for x in box[di]],
-                                  'score': floatn(d[4], 5)})
-
-            # Clip boxes to image bounds
-            clip_coords(pred, (height, width))
-
-            # Assign all predictions as incorrect
-            correct = [0] * len(pred)
-            if nl:
-                detected = []
-                tcls_tensor = labels[:, 0]
-
-                # target boxes
-                tbox = xywh2xyxy(labels[:, 1:5])
-                tbox[:, [0, 2]] *= width
-                tbox[:, [1, 3]] *= height
-
-                # Search for correct predictions
-                for i, (*pbox, pconf, pcls_conf, pcls) in enumerate(pred):
-
-                    # Break if all targets already located in image
-                    if len(detected) == nl:
-                        break
-
-                    # Continue if predicted class not among image classes
-                    if pcls.item() not in tcls:
-                        continue
-
-                    # Best iou, index between pred and targets
-                    m = (pcls == tcls_tensor).nonzero().view(-1)
-                    iou, bi = bbox_iou(pbox, tbox[m]).max(0)
-
-                    # If iou > threshold and class is correct mark as correct
-                    if iou > iou_thres and m[bi] not in detected:  # and pcls == tcls[bi]:
-                        correct[i] = 1
-                        detected.append(m[bi])
-
-            # Append statistics (correct, conf, pcls, tcls)
-            stats.append((correct, pred[:, 4].cpu(), pred[:, 6].cpu(), tcls))
+                    detected = []
+                    tcls_tensor = labels[:, 0]
+    
+                    # target boxes
+                    tbox = xywh2xyxy(labels[:, 1:5])
+                    tbox[:, [0, 2]] *= width
+                    tbox[:, [1, 3]] *= height
+    
+                    # Search for correct predictions
+                    for i, (*pbox, pconf, pcls_conf, pcls) in enumerate(pred):
+    
+                        # Break if all targets already located in image
+                        if len(detected) == nl:
+                            break
+    
+                        # Continue if predicted class not among image classes
+                        if pcls.item() not in tcls:
+                            continue
+    
+                        # Best iou, index between pred and targets
+                        m = (pcls == tcls_tensor).nonzero().view(-1)
+                        iou, bi = bbox_iou(pbox, tbox[m]).max(0)
+    
+                        # If iou > threshold and class is correct mark as correct
+                        if iou > iou_thres and m[bi] not in detected:  # and pcls == tcls[bi]:
+                            correct[i] = 1
+                            detected.append(m[bi])
+    
+                # Append statistics (correct, conf, pcls, tcls)
+                stats.append((correct, pred[:, 4].cpu(), pred[:, 6].cpu(), tcls))
 
     # Compute statistics
     stats = [np.concatenate(x, 0) for x in list(zip(*stats))]  # to numpy
@@ -204,13 +206,12 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     print(opt)
 
-    with torch.no_grad():
-        results = test(opt.cfg,
-                       opt.data,
-                       opt.weights,
-                       opt.batch_size,
-                       opt.img_size,
-                       opt.iou_thres,
-                       opt.conf_thres,
-                       opt.nms_thres,
-                       opt.save_json)
+    results = test(opt.cfg,
+                   opt.data,
+                   opt.weights,
+                   opt.batch_size,
+                   opt.img_size,
+                   opt.iou_thres,
+                   opt.conf_thres,
+                   opt.nms_thres,
+                   opt.save_json)
