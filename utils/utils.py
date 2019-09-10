f270269d437280b35c64b72048cc43cbea67fd75
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-09-10 12:20:59 +0200

    updates

--- utils/utils.py
+++ utils/utils.py
@@ -674,7 +674,7 @@ def kmeans_targets(path='../coco/trainvalno5k.txt', n=9, img_size=416):  # from 
 def print_mutation(hyp, results, bucket=''):
     # Print mutation results to evolve.txt (for use with train.py --evolve)
     a = '%10s' * len(hyp) % tuple(hyp.keys())  # hyperparam keys
-    b = '%10.3g' * len(hyp) % tuple(hyp.values())  # hyperparam values
+    b = '%10.5g' * len(hyp) % tuple(hyp.values())  # hyperparam values
     c = '%10.3g' * len(results) % results  # results (P, R, mAP, F1, test_loss)
     print('\n%s\n%s\nEvolved fitness: %s\n' % (a, b, c))
 
