96c442c3c30a554701c0ddcf4d121c33fd820f76
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-09-20 15:23:08 +0200

    updates

--- detect.py
+++ detect.py
@@ -131,7 +131,7 @@ if __name__ == '__main__':
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='cfg file path')
     parser.add_argument('--data', type=str, default='data/coco.data', help='coco.data file path')
     parser.add_argument('--weights', type=str, default='weights/yolov3-spp.weights', help='path to weights file')
-    parser.add_argument('--source', type=str, default='rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa', help='source')  # input file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
