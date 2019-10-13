376e00a3cf496364b20f9632462d7ddbe4bcb3c6
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-10-13 18:53:15 +0200

    updates

--- train.py
+++ train.py
@@ -366,6 +366,7 @@ def train():
     # end training
     if len(opt.name):
         os.rename('results.txt', 'results_%s.txt' % opt.name)
+        os.rename(wdir + 'best.pt', wdir + 'best_%s.pt' % opt.name)
     plot_results()  # save as results.png
     print('%g epochs completed in %.3f hours.\n' % (epoch - start_epoch + 1, (time.time() - t0) / 3600))
     dist.destroy_process_group() if torch.cuda.device_count() > 1 else None
