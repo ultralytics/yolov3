0245ff913333223a3379bf06da2d25fb2a77eee8
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-11-25 08:26:26 -1000

    updates

--- utils/utils.py
+++ utils/utils.py
@@ -240,12 +240,10 @@ def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False):
     box2 = box2.t()
 
     # Get the coordinates of bounding boxes
-    if x1y1x2y2:
-        # x1, y1, x2, y2 = box1
+    if x1y1x2y2:  # x1, y1, x2, y2 = box1
         b1_x1, b1_y1, b1_x2, b1_y2 = box1[0], box1[1], box1[2], box1[3]
         b2_x1, b2_y1, b2_x2, b2_y2 = box2[0], box2[1], box2[2], box2[3]
-    else:
-        # x, y, w, h = box1
+    else:  # x, y, w, h = box1
         b1_x1, b1_x2 = box1[0] - box1[2] / 2, box1[0] + box1[2] / 2
         b1_y1, b1_y2 = box1[1] - box1[3] / 2, box1[1] + box1[3] / 2
         b2_x1, b2_x2 = box2[0] - box2[2] / 2, box2[0] + box2[2] / 2
