92690302bb939d18973a1d3ae2f6ed9b59644dd2
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-12-01 13:49:38 -0800

    updates

--- train.py
+++ train.py
@@ -43,6 +43,7 @@ hyp = {'giou': 3.31,  # giou loss gain
 # Overwrite hyp with hyp*.txt (optional)
 f = glob.glob('hyp*.txt')
 if f:
+    print('Using %s' % f)
     for k, v in zip(hyp.keys(), np.loadtxt(f[0])):
         hyp[k] = v
 
