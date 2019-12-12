b87bfa32c36f582d21ac3da7b21d9d9178d339ba
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-12-12 13:56:56 -0800

    updates

--- detect.py
+++ detect.py
@@ -125,6 +125,8 @@ def detect(save_txt=False, save_img=False):
             # Stream results
             if view_img:
                 cv2.imshow(p, im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
 
             # Save results (image with detections)
             if save_img:
