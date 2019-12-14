4b368b704b07cd9dcdee7a80120cb808a72b1a45
Glenn Jocher <glenn.jocher@ultralytics.com>
2019-12-13 17:34:19 -0800

    updates

--- data/get_coco2017.sh
+++ data/get_coco2017.sh
@@ -8,7 +8,7 @@ filename="coco2017labels.zip"
 fileid="1cXZR_ckHki6nddOmcysCuuJFM--T-Q6L"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-rm ./cookie ./$filename
+rm ./cookie
 
 # Unzip labels
 unzip -q ${filename}  # for coco.zip
@@ -24,7 +24,7 @@ unzip -q train2017.zip
 unzip -q val2017.zip
 
 # (optional) Delete zip files
-rm -rf *.zip
+rm -rf *.zip ../*.zip
 
 # cd out
 cd ../..
