4b56a370e6af1ccabc69398954c3549d842aae93
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-01-11 13:13:57 -0800

    updates

--- utils/datasets.py
+++ utils/datasets.py
@@ -499,7 +499,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
     @staticmethod
     def collate_fn(batch):
-        img, label, path, shapes = list(zip(*batch))  # transposed
+        img, label, path, shapes = zip(*batch)  # transposed
         for i, l in enumerate(label):
             l[:, 0] = i  # add target image index for build_targets()
         return torch.stack(img, 0), torch.cat(label, 0), path, shapes
