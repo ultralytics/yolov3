dd3cf27ececafc17136cce82c8dd502ce4dae6d0
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-01-23 17:26:05 -0800

    updates

--- utils/evolve.sh
+++ utils/evolve.sh
@@ -6,7 +6,7 @@
 #done
 
 while true; do
-  python3 train.py --data ../data/sm4/out.data --img-size 320 --epochs 100 --batch 64 --accum 1 --weights yolov3-tiny.pt --multi --bucket ult/wer --evolve --cache --device $1 --cfg yolov3-tiny3-1cls.cfg --single --adam
+  python3 train.py --data ../data/sm4/out.data --img-size 320 --epochs 100 --batch 64 --accum 1 --weights yolov3-tiny.conv.15 --multi --bucket ult/wer --evolve --cache --device $1 --cfg yolov3-tiny3-1cls.cfg --single --adam
   # python3 train.py --data ../out/data.data --img-size 608 --epochs 10 --batch 8 --accum 8 --weights ultralytics68.pt --multi --bucket ult/athena --evolve --device $1 --cfg yolov3-spp-1cls.cfg --adam
 
   # python3 train.py --data coco2014.data --img-size 640 --epochs 10 --batch 22 --accum 3 --evolve --weights '' --bucket yolov4/640ms_coco2014_10e --device $1 --multi
