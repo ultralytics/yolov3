a9240ee17d40782785b8a989d9b06f06545a5718
LinJian <linjian3@corp.netease.com>
2020-01-31 11:48:57 +0800

    fusedconv bug fix, https://github.com/ultralytics/yolov3/issues/807

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -61,7 +61,7 @@ def fuse_conv_and_bn(conv, bn):
         else:
             b_conv = torch.zeros(conv.weight.size(0))
         b_bn = bn.bias - bn.weight.mul(bn.running_mean).div(torch.sqrt(bn.running_var + bn.eps))
-        fusedconv.bias.copy_(b_conv + b_bn)
+        fusedconv.bias.copy_(torch.mm(w_bn, b_conv.reshape(-1, 1)).reshape(-1) + b_bn)
 
         return fusedconv
 
