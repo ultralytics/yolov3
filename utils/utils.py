ca4960f7ff7667995ce1df3413a1d618bc8f24c9
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-02-08 13:28:47 -0800

    updates

--- utils/utils.py
+++ utils/utils.py
@@ -878,7 +878,7 @@ def apply_classifier(x, model, img, im0):
 
 def fitness(x):
     # Returns fitness (for use with results.txt or evolve.txt)
-    w = [0.0, 0.01, 0.99, 0.0]  # weights for [P, R, mAP, F1]@0.5 or [P, R, mAP@0.5:0.95, mAP@0.5]
+    w = [0.0, 0.01, 0.99, 0.00]  # weights for [P, R, mAP, F1]@0.5 or [P, R, mAP@0.5, mAP@0.5:0.95]
     return (x[:, :4] * w).sum(1)
 
 
