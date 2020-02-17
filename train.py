cca620208eff32f40c857a2b6ea5d6bd7919264e
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-02-16 23:13:34 -0800

    updates

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ results_file = 'results.txt'
 hyp = {'giou': 3.54,  # giou loss gain
        'cls': 37.4,  # cls loss gain
        'cls_pw': 1.0,  # cls BCELoss positive_weight
-       'obj': 49.5,  # obj loss gain (*=img_size/320 if img_size != 320)
+       'obj': 64.3,  # obj loss gain (*=img_size/320 if img_size != 320)
        'obj_pw': 1.0,  # obj BCELoss positive_weight
        'iou_t': 0.225,  # iou training threshold
        'lr0': 0.00579,  # initial learning rate (SGD=5E-3, Adam=5E-4)
