dab926cc06657fd5d20f821435b26061e6d4464e
jzisheng <jzisheng@gmail.com>
2020-03-10 13:05:25 +0000

    updated gitignore

--- train.sh
+++
@@ -1,4 +0,0 @@
-python3 train.py --data '/data/zjc4/chipped-30/xview_data.txt' --epochs=1100 --resume
-mv weights/best.pt weights/best-30.pt
-#python3 train.py --data '/data/zjc4/chipped-90/xview_data.txt' --epochs=200 --resume
-#mv weights/best.pt weights/best-90.pt
