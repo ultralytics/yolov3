0de07da61287f764cce440986ddf8ff6d4d3a8ff
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-03-13 11:03:39 -0700

    updates

--- utils/utils.py
+++ utils/utils.py
@@ -325,7 +325,7 @@ def box_iou(boxes1, boxes2):
     lt = torch.max(boxes1[:, None, :2], boxes2[:, :2])  # [N,M,2]
     rb = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])  # [N,M,2]
 
-    inter = (rb - lt).clamp(min=0).prod(2)  # [N,M]
+    inter = (rb - lt).clamp(0).prod(2)  # [N,M]
     return inter / (area1[:, None] + area2 - inter)  # iou = inter / (area1 + area2 - inter)
 
 
