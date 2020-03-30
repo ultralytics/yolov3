108334db29e45a80f43b12a8dde24712257290cc
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-03-30 16:04:08 -0700

    FLOPs update

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -89,9 +89,9 @@ def model_info(model, verbose=False):
                   (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
     print('Model Summary: %g layers, %g parameters, %g gradients' % (len(list(model.parameters())), n_p, n_g))
 
-    # FLOPS report
+    # FLOPs
     # from thop import profile
-    # macs, params = profile(model, inputs=(torch.zeros(1, 3, 608, 608),))
+    # macs, params = profile(model, inputs=(torch.zeros(1, 3, 640, 640),))
     # print('%.3f GFLOPs' % (macs / 1E9 * 2))
 
 
