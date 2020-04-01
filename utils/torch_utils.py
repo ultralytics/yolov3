4ac60018f6e6c1e24b496485f126a660d9c793d8
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-04-01 14:05:41 -0700

    FLOPS report

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -87,12 +87,15 @@ def model_info(model, verbose=False):
             name = name.replace('module_list.', '')
             print('%5g %40s %9s %12g %20s %10.3g %10.3g' %
                   (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
-    print('Model Summary: %g layers, %g parameters, %g gradients' % (len(list(model.parameters())), n_p, n_g))
 
-    # FLOPs
-    # from thop import profile
-    # macs, params = profile(model, inputs=(torch.zeros(1, 3, 640, 640),))
-    # print('%.3f GFLOPs' % (macs / 1E9 * 2))
+    try:  # FLOPS
+        from thop import profile
+        macs, _ = profile(model, inputs=(torch.zeros(1, 3, 640, 640),))
+        fs = ', %.1f GFLOPS' % (macs / 1E9 * 2)
+    except:
+        fs = ''
+
+    print('Model Summary: %g layers, %g parameters, %g gradients%s' % (len(list(model.parameters())), n_p, n_g, fs))
 
 
 def load_classifier(name='resnet101', n=2):
