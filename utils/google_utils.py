1bbbbfe2d8c62cc63f5ccb1949618ed5e632ffd2
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-20 12:59:15 +0100

    Remove redundant download mirror

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -22,8 +22,18 @@ def attempt_download(weights):
 
     msg = weights + ' missing, try downloading from https://github.com/ultralytics/yolov3/releases/'
     models = ['yolov3.pt', 'yolov3-spp.pt', 'yolov3-tiny.pt']  # available models
+    redundant = False  # offer second download option
 
     if file in models and not os.path.isfile(weights):
+        # Google Drive
+        # d = {'yolov5s.pt': '1R5T6rIyy3lLwgFXNms8whc-387H0tMQO',
+        #      'yolov5m.pt': '1vobuEExpWQVpXExsJ2w-Mbf3HJjWkQJr',
+        #      'yolov5l.pt': '1hrlqD1Wdei7UT4OgT785BEk1JwnSvNEV',
+        #      'yolov5x.pt': '1mM8aZJlWTxOg7BZJvNUMrTnA2AbeCVzS'}
+        # r = gdrive_download(id=d[file], name=weights) if file in d else 1
+        # if r == 0 and os.path.exists(weights) and os.path.getsize(weights) > 1E6:  # check
+        #    return
+
         try:  # GitHub
             url = 'https://github.com/ultralytics/yolov3/releases/download/v9.0/' + file
             print('Downloading %s to %s...' % (url, weights))
@@ -31,6 +41,7 @@ def attempt_download(weights):
             assert os.path.exists(weights) and os.path.getsize(weights) > 1E6  # check
         except Exception as e:  # GCP
             print('Download error: %s' % e)
+            assert redundant, 'No secondary mirror'
             url = 'https://storage.googleapis.com/ultralytics/yolov3/ckpt/' + file
             print('Downloading %s to %s...' % (url, weights))
             r = os.system('curl -L %s -o %s' % (url, weights))  # torch.hub.download_url_to_file(url, weights)
