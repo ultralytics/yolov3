2958149707d0b36ee0cb5b0fbc6f51c4aaeb65bd
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-12 15:59:10 +0200

    Conv act LeakyReLU(0.1)

--- models/common.py
+++ models/common.py
@@ -36,7 +36,7 @@ class Conv(nn.Module):
         super(Conv, self).__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+        self.act = nn.LeakyReLU(0.1) if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
 
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
