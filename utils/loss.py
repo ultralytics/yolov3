609d1e4f109984c745a80297bbaba375270892fa
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-29 15:56:07 +0200

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 
 from utils.metrics import bbox_iou
-from utils.torch_utils import is_parallel
+from utils.torch_utils import de_parallel
 
 
 def smooth_BCE(eps=0.1):  # https://github.com/ultralytics/yolov3/issues/238#issuecomment-598028441
