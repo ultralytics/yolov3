d835bf585c0c4588bb04b162f10ec3c7c1c0acc0
Ultralytics Assistant <135830346+UltralyticsAssistant@users.noreply.github.com>
2024-06-08 22:02:15 +0200

    Add license line to models/tf.py

--- models/tf.py
+++ models/tf.py
@@ -1,4 +1,5 @@
-# YOLOv3 🚀 by Ultralytics, AGPL-3.0 license
+# Ultralytics YOLOv3 🚀, AGPL-3.0 license
+
 """
 TensorFlow, Keras and TFLite versions of YOLOv3
 Authored by https://github.com/zldrobit in PR https://github.com/ultralytics/yolov5/pull/1127
